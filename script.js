4e6cdbf07dc6ea1da9553659c7234176bc847acb
karlitomasterini-dotcom <karlito.masterini@gmail.com>
2026-01-30 19:04:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,13 @@ let tg = window.Telegram.WebApp;
 tg.ready();
 
 function sendForm() {
-    alert("КНОПКА НАЖАЛАСЬ ✅");
-
     let data = {
         name: document.getElementById("name").value,
         phone: document.getElementById("phone").value,
         area: document.getElementById("area").value
     };
 
-    alert("ДАННЫЕ СОБРАНЫ ✅");
+    tg.sendData(JSON.stringify(data));  // Отправка данных в Telegram
 
-    tg.sendData(JSON.stringify(data));
-
-    alert("ДАННЫЕ ОТПРАВЛЕНЫ В TELEGRAM ✅");
-
-    tg.close();
+    tg.close();  // Закрытие мини-приложения
 }
